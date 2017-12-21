c3ebb5c38028183a91464a73a607b9c12f63781c
zhuweiyou <i@zhuweiyou.com>
2017-12-21 11:51:08 +0800

    add taobao rule

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,7 +1,8 @@
 const rules = [
-  {name: '腾讯', domain: 'qq.com', length: 16},
-  {name: 'github', domain: 'github.com', length: 72},
-  {name: 'npm', domain: 'npmjs.com', length: 72},
+  {name: '腾讯QQ', domain: 'qq.com', length: 16},
+  {name: '微信', domain: 'wx.qq.com', length: 16},
+  {name: 'GitHub', domain: 'github.com', length: 72},
+  {name: 'NPM', domain: 'npmjs.com', length: 72},
   {name: '知乎', domain: 'zhihu.com', length: 128},
   {name: '百度', domain: 'baidu.com', length: 14},
   {name: '新浪微博', domain: 'weibo.com', length: 16},
@@ -9,6 +10,7 @@ const rules = [
   {name: '豆瓣', domain: 'douban.com', length: 20},
   {name: '拉钩', domain: 'lagou.com', length: 16},
   {name: '阿里云企业邮', domain: 'qiye.aliyun.com', length: 32},
+  {name: '淘宝', domain: 'taobao.com', length: 20},
 ]
 
 rules.sort((a, b) => a.domain <= b.domain ? -1 : 1)
