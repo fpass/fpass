f8b40fd667c504887136cdc97496cc70043d0c88
zhuweiyou <i@zhuweiyou.com>
2017-12-21 11:22:03 +0800

    add rules

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,6 +1,14 @@
 const rules = [
   {name: '腾讯', domain: 'qq.com', length: 16},
-  {name: 'GitHub', domain: 'github.com', length: 72},
+  {name: 'github', domain: 'github.com', length: 72},
+  {name: 'npm', domain: 'npmjs.com', length: 72},
+  {name: '知乎', domain: 'zhihu.com', length: 128},
+  {name: '百度', domain: 'baidu.com', length: 14},
+  {name: '新浪微博', domain: 'weibo.com', length: 16},
+  {name: '12306', domain: '12306.cn', length: 20},
+  {name: '豆瓣', domain: 'douban.com', length: 20},
+  {name: '拉钩', domain: 'lagou.com', length: 16},
+  {name: '阿里云企业邮', domain: 'qiye.aliyun.com', length: 32},
 ]
 
 rules.sort((a, b) => a.domain <= b.domain ? -1 : 1)
