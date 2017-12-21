a0592b04bff37b2c39eb7ed5354c590aeffbfbe9
duminghong <i@duminghong.com>
2017-12-21 13:15:35 +0800

    新增斗鱼、哔哩哔哩

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,7 +1,8 @@
 const rules = [
-  {name: '腾讯', domain: 'qq.com', length: 16},
-  {name: 'github', domain: 'github.com', length: 72},
-  {name: 'npm', domain: 'npmjs.com', length: 72},
+  {name: '腾讯QQ', domain: 'qq.com', length: 16},
+  {name: '微信', domain: 'wx.qq.com', length: 16},
+  {name: 'GitHub', domain: 'github.com', length: 72},
+  {name: 'NPM', domain: 'npmjs.com', length: 72},
   {name: '知乎', domain: 'zhihu.com', length: 128},
   {name: '百度', domain: 'baidu.com', length: 14},
   {name: '新浪微博', domain: 'weibo.com', length: 16},
@@ -9,6 +10,11 @@ const rules = [
   {name: '豆瓣', domain: 'douban.com', length: 20},
   {name: '拉钩', domain: 'lagou.com', length: 16},
   {name: '阿里云企业邮', domain: 'qiye.aliyun.com', length: 32},
+  {name: '淘宝', domain: 'taobao.com', length: 20},
+  {name: '京东', domain: 'jd.com', length: 20},
+  {name: 'Coding', domain: 'coding.net', length: 64},
+  {name: '开源中国', domain: 'oschina.net', length: 20},
+  {name: '码云', domain: 'gitee.com', length: 16},
   {name: '斗鱼', domain: 'douyu.com', length: 25},
   {name: '哔哩哔哩', domain: 'bilibili.com', length: 16},
 ]
