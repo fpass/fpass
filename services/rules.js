70f7e0b825b485715a829eeb250fb5dfd73e5afa
zhuweiyou <i@zhuweiyou.com>
2017-12-24 21:14:35 +0800

    feat(rules): add freenom.com

--- services/rules.js
+++ services/rules.js
@@ -21,7 +21,8 @@ const rules = [
   {name: '哔哩哔哩', domain: 'bilibili.com', length: 16},
   {name: '甲骨文', domain: 'oracle.com', length: 80},
   {name: '饿了么', domain: 'ele.me', length: 20},
-  {name: 'CloudFlare', domain: 'cloudflare.com', length: 128},
+  {name: 'Cloudflare', domain: 'cloudflare.com', length: 128},
+  {name: 'Freenom', domain: 'freenom.com', length: 128},
 ]
 
 rules.sort((a, b) => a.domain <= b.domain ? -1 : 1)
