8c54e2a07a3e7e775dbd2ad03d79c8164691e290
zhuweiyou <i@zhuweiyou.com>
2017-12-26 13:52:47 +0800

    refactor: move files

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ module.exports = {
   env: {
     VERSION: isProduction ? JSON.parse(execSync('curl -s https://api.github.com/repos/fpass/fpass/tags').toString())[0].name : ''
   },
+  srcDir: 'src/',
   loading: false,
   modules: [
     '~/modules/pwa'
