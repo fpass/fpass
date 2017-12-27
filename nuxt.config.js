a16c7d9755b5f3a9f3fe4bf54f3e56b99b9f506e
zhuweiyou <i@zhuweiyou.com>
2017-12-27 14:30:56 +0800

    ci: master auto build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,6 @@
 const execSync = require('child_process').execSync
-const isProduction = process.env.NODE_ENV === 'production'
-
-let RULES = [{name: '测试1', domain: 'test1.com', length: 16}, {name: '测试2', domain: 'test2.com', length: 8}]
-let VERSION = ''
-if (isProduction) {
-  RULES = eval(JSON.parse(execSync('curl -s https://api.github.com/repos/fpass/fpass/issues/3')).body.match(/```js([\s\S]*?)```/).pop())
-  VERSION = JSON.parse(execSync('curl -s https://api.github.com/repos/fpass/fpass/tags'))[0].name
-}
 
 module.exports = {
-  env: {
-    RULES,
-    VERSION
-  },
   srcDir: 'src/',
   loading: false,
   modules: [
