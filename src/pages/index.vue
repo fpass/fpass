ec48fe928b27662b6d59b9978c9235cbfe934d22
zhuweiyou <i@zhuweiyou.com>
2018-01-07 15:39:43 +0800

    修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
 
 </template>
+
+<script>
+  export default {
+    asyncData ({redirect, store, isStatic}) {
+      redirect(`/${store.state.locale}`)
+    }
+  }
+</script>
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,25 +3,15 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-export default ({app, route, params, store}) => {
+export default ({app, isStatic, params, route, store}) => {
   app.i18n = new VueI18n({
     locale: store.state.locale,
-    fallbackLocale: 'en',
     messages: {
       'en': require('../locales/en.json'),
       'zh-CN': require('../locales/zh-CN.json')
     }
   })
-  app.i18n.path = link =>
-    app.i18n.locale === app.i18n.fallbackLocale
-      ? `/${link}`
-      : `/${app.i18n.locale}/${link}`
-
-  const locale = params.lang || (typeof window !== 'undefined' ? navigator.language : '') || 'en'
+  const locale = params.lang || route.query.lang || (isStatic ? navigator.language : '') || 'en'
   store.commit('setLang', locale)
   app.i18n.locale = store.state.locale
-
-  if (route.path === '/') {
-    app.router.push(locale)
-  }
 }
