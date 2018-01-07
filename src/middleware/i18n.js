89415cb9cf977083195fea7bd09aaee80582272a
zhuweiyou <i@zhuweiyou.com>
2018-01-07 17:07:13 +0800

    更新逻辑

--- src/middleware/i18n.js
+++ src/middleware/i18n.js
@@ -2,16 +2,10 @@ export default function ({isHMR, app, store, route, params, redirect}) {
   if (isHMR) {
     return
   }
-
   const locale = params.lang || route.query.lang || (typeof window !== 'undefined' ? navigator.language : '') || 'en'
-  if (!store.state.locales.includes(locale)) {
-    return redirect('/')
-  }
-
-  store.commit('setLang', locale)
-  app.i18n.locale = store.state.locale
-
   if (route.path === '/') {
     return redirect(`/${locale}`)
   }
+  store.commit('setLang', locale)
+  app.i18n.locale = store.state.locale
 }
