7595e1e4af634caae48c0042ece3de1ceb5c26bb
zhuweiyou <i@zhuweiyou.com>
2018-01-27 20:40:08 +0800

    更新

--- src/page/404.vue
+++ src/page/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrap">
     页面没有找到，点击
-    <router-link :to="{path: '/'}">返回首页</router-link>
+    <router-link to="/">返回首页</router-link>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import { locales } from '../i18n'
+import { i18n, locales } from '../i18n'
 import PageIndex from '../page/index'
 import Page404 from '../page/404'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: [
     {
+      name: 'index',
       path: '/',
       component: PageIndex
     },
     ...locales.map(locale => ({
+      name: locale,
       path: `/${locale}`,
       component: PageIndex
     })),
     {
+      name: '404',
       path: '*',
       component: Page404
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (locales.indexOf(to.name) !== -1) {
+    i18n.locale = to.name
+  }
+  next()
+})
+
+export default router
