a8606d1afedde6ae439ef95253f20911595e7cf1
zhuweiyou <i@zhuweiyou.com>
2018-01-27 19:43:35 +0800

    更新

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,7 +3,9 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
+const locales = ['en', 'zh-CN']
+
 export default new VueI18n({
-  locale: navigator.language || 'en',
-  messages: ['en', 'zh-CN'].reduce((obj, locale) => (obj[locale] = require(`./${locale}.json`), obj), {})
+  locale: locales.indexOf(navigator.language) === -1 ? 'en' : navigator.language,
+  messages: locales.reduce((obj, locale) => (obj[locale] = require(`./${locale}.json`), obj), {})
 })
