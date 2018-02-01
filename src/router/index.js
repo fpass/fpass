39e13cde1f1c52ef5e37a9e70f0eaaff7e2d5ece
zhuweiyou <i@zhuweiyou.com>
2018-02-01 16:57:42 +0800

    不支持的语言走默认

--- src/router/index.js
+++ src/router/index.js
@@ -2,38 +2,33 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { i18n, locales } from '../i18n'
 import PageIndex from '../page/index'
-import Page404 from '../page/404'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    {
-      name: 'index',
-      path: '/',
-      component: PageIndex
-    },
     ...locales.map(locale => ({
       name: locale,
       path: `/${locale}`,
       component: PageIndex
     })),
     {
-      name: '404',
+      name: 'all',
       path: '*',
-      component: Page404
+      component: PageIndex
     }
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  if (to.name === 'index') {
-    return next(navigator.language)
-  }
-  i18n.locale = locales.indexOf(to.name) === -1
+router.beforeEach(({name}, from, next) => {
+  const locale = locales.indexOf(name) === -1
     ? (locales.indexOf(navigator.language) === -1 ? locales[0] : navigator.language)
-    : to.name
+    : name
+  if (name === 'all') {
+    return next(locale)
+  }
+  i18n.locale = locale
   next()
 })
 
