bc9229c6e8eb6256290949db82bf0a3e82c74693
zhuweiyou <i@zhuweiyou.com>
2018-02-02 10:23:27 +0800

    跳转语言不产生新的历史记录

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,10 @@ router.beforeEach(({name}, from, next) => {
     ? (locales.indexOf(navigator.language) === -1 ? locales[0] : navigator.language)
     : name
   if (name === 'index') {
-    return next(locale)
+    return next({
+      name: locale,
+      replace: true
+    })
   }
   i18n.locale = locale
   next()
